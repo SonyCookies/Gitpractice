522edb3468886c69d3c1d0493bdd307363a9b860
SonyCookies <toxicookie.v1@gmail.com>
2021-09-12 02:42:42 +0800

    change 1

--- app/src/main/java/com/sonnyapps/gitpractice/MainActivity.kt
+++ app/src/main/java/com/sonnyapps/gitpractice/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         println("Hello")
         println("Local change")
 
-        println("EXperimental")
+        println("change 1")
         println("ddeeed")
     }
 }
